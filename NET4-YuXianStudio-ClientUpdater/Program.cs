d275c31458498af1ee4d630dff7b8d7a1a5a3f22
PascalYuxian <343685355@qq.com>
2018-10-28 00:05:13 +0800

    添加项目文件。

---
+++ NET4-YuXianStudio-ClientUpdater/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NET4_YuXianStudio_ClientUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
